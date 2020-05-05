e317740c97a3c51d9f2e6c387dc5a54b9e728837
s181034-MoriJellyfish <52020604+s181034-MoriJellyfish@users.noreply.github.com>
2020-05-06 00:23:14 +0900

    Upload to Github

---
+++ Assets/Scripts/InputManage.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class InputManage : MonoBehaviour
+{
+    Color TapColor=new Color(1,1,1,1);
+    Color NormalColor=new Color(1,1,1,0.5f);
+    [SerializeField] KeyCode key;
+    [SerializeField] GameObject Line;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(key))
+        {
+            GetComponent<SpriteRenderer>().color = TapColor;
+            if (Line.transform.childCount != 0)
+            {
+                GameObject obj= Line.transform.GetChild(0).gameObject;
+                var i = Math.Abs(obj.transform.localPosition.y);
+                if (i <= 0.2)
+                {
+                    Debug.Log("Pure");
+                    Score.score += 2;
+                    Destroy(obj);
+                }else if (i < 0.4)
+                {
+                    Debug.Log("Far");
+                    Score.score += 1;
+                    Destroy(obj);
+                }
+
+                
+            }
+
+        }
+
+        if (!Input.GetKey(key))
+        {
+            GetComponent<SpriteRenderer>().color = NormalColor;
+        }
+    }
+}
---
+++ Assets/Scripts/NoteMain.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteMain : MonoBehaviour
+{
+    [SerializeField] GameObject missEff;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 0.1f, 0);
+        if (transform.localPosition.y >= 0.4f)
+        {
+            missEff.transform.position = this.transform.position;
+            missEff.transform.rotation = this.transform.rotation;
+            Instantiate(missEff);
+            Destroy(this.gameObject);
+            Debug.Log("miss");
+        }
+    }
+}
