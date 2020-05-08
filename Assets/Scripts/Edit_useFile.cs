cdea8355b2bf7ac31feba03bdb3f1f443d92f33f
s181034-MoriJellyfish <52020604+s181034-MoriJellyfish@users.noreply.github.com>
2020-05-08 15:48:02 +0900

    Output File

---
+++ Assets/Scripts/Edit_useFile.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Edit_useFile : MonoBehaviour
+{
+
+    string humen="";
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A)){
+            WriteFile();
+        }
+    }
+    void WriteFile()
+    {
+        humen = "";
+        foreach (int tmp in Edit_HumenData.humen)
+        {
+            humen += tmp.ToString();
+            humen += ",";
+        }
+        File.WriteAllText(@".\test.txt", humen);
+
+    }
+}
